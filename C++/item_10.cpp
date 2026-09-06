7885b5982c892159541d5131172d91b53042eb6a
pawit <toppavit@Toppavit.toppavit>
2026-09-06 03:28:12 -0400

    copy constructor practice

--- C++/item_10.cpp
+++ C++/item_10.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <string_view>
+#include <string>
+#include <memory>
+#include <charconv> 
 #include <print>
 
 
-
+class Myclass {
+    
+public:
+       Myclass(int value,std::string text) : m_value(value) , m_string(text) 
+    {
+        std::print("parameter constructor called\n");
+    }    
+       Myclass(const Myclass& value):
+           m_value(value.m_value), m_string(value.m_string) 
+    {
+        std::print("Copy constructor had been call\n");
+    } 
+private:
+       int m_value {0} ;
+       std::string m_string;
+}; 
 
 
 
@@ -11,7 +30,11 @@
 
 int main () {
 using namespace std;
-       
+    
+   Myclass src {10 , "Top"};
+   Myclass dest = src;
+   
+
 
 
 
